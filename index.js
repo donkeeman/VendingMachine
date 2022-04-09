cafa73bad3624a4cee20442819b9d209479d4168
lhw421 <gpdnjs421@gmail.com>
2022-04-09 13:12:12 +0900

    기본 구조 잡기

---
+++ index.js
@@ -0,0 +1,8 @@
+// 추후에는 json 파일로 받아 온다.
+
+const drinkNameList = ["Original_Cola", "Violet_Cola", ""]
+
+let drinks = document.getElementById("drinksTable").childNodes;
+
+for(let i = 0; i<drinks.length; i++){
+}
