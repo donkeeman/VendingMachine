1be6fc79ef14932b7049ece3b80d0966bdb37d3b
lhw421 <gpdnjs421@gmail.com>
2022-04-21 08:57:00 +0900

    주석 추가 중

--- index.js
+++ index.js
@@ -23,30 +23,36 @@ let ownColaCount = [0, 0, 0, 0, 0, 0];
 
 let totalCount = 0;
 
-// 콜라 클릭 시 구매 리스트에 추가
+// 콜라 버튼 클릭 시 구매 리스트에 추가
 for(let i = 0; i<cola.length; i++){
     cola[i].onclick = function(){
+        // 클릭 시 클릭된 상태를 표시하는 clicked 클래스를 버튼에 추가
         cola[i].classList.add("clicked");
-        // if 콜라 재고가 0개가 아닐 때
+
         // 클릭할 때마다 획득하는 콜라의 개수는 1 증가
         buyColaCount[i]++;
         setColaCount(buyColaList, buyColaCount, i);
 
-        // 재고는 1 감소
+        // 콜라의 재고는 1 감소
         colaCount[i]--;
+
+        // 재고가 0이면 품절 상태를 표시하는 soldout 클래스를 버튼에 추가
         if(colaCount[i] == 0)
             this.classList.add("soldout");
     }
 }
 
-// 구매 리스트의 콜라 버튼 클릭 시 리스트에서 콜라 제거
+// 구매 리스트의 콜라 버튼 클릭 시 리스트에서 콜라 환불
 for(let i = 0; i<buyColaList.childElementCount; i++){
     buyColaList.children[i].lastElementChild.onclick = function(){
         // 클릭할 때마다 획득한 콜라의 수에서 1 감소
         buyColaCount[i]--;
         setColaCount(buyColaList, buyColaCount, i);
+
         // 콜라 재고는 1 증가
         colaCount[i]++;
+
+        // 콜라의 재고가 0이었다가 다시 1개 이상 생긴 경우 soldout 클래스를 제거하고 원래대로 되돌림
         if(colaCount[i] != 0)
             cola[i].classList.remove("soldout");
     }
@@ -54,10 +60,10 @@ for(let i = 0; i<buyColaList.childElementCount; i++){
 
 returnButton.onclick = returnMoney;
 depositButton.onclick = depositMoney;
-buyButton.onclick = buyDrink;
+buyButton.onclick = buyCola;
 
-function buyDrink(){
-    // 선택한 음료수의 총 개수 구하기
+function buyCola(){
+    // 선택한 콜라의 총 개수 최종 확정
     totalCount = 0;
     for(let i = 0; i<buyColaCount.length; i++)
         totalCount += buyColaCount[i];
@@ -67,14 +73,21 @@ function buyDrink(){
         alert("잔액이 부족합니다. 입금 후 다시 시도해주세요.");
     // 잔액 안 부족하면
     else{
+        // 잔액 = 잔액 - 음료수 구매액
         balance.innerHTML = parseInt(balance.innerHTML) - 1000*totalCount;
-        total.innerHTML = parseInt(total.innerHTML)+1000*totalCount;
+        // 총금액 = 총금액 + 음료수 구매액
+        total.innerHTML = parseInt(total.innerHTML) + 1000*totalCount;
 
         for(let i = 0; i<ownColaCount.length; i++){
+            // buyColaCount에 저장했던 콜라 구매 개수를 ownColaCount에 누적
             ownColaCount[i] += buyColaCount[i];
+            // buyColaCount는 초기화
             buyColaCount[i] = 0;
+
             setColaCount(ownColaList, ownColaCount, i);
             setColaCount(buyColaList, buyColaCount, i);
+
+            // 콜라 버튼에서 clicked 클래스 제거
             cola[i].classList.remove("clicked");
         }
     }
