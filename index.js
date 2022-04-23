941b38e13a79482b28179c87f3140c179e8df13b
lhw421 <gpdnjs421@gmail.com>
2022-04-23 22:08:53 +0900

    주석 추가

--- index.js
+++ index.js
@@ -64,16 +64,18 @@ returnButton.onclick = returnMoney;
 depositButton.onclick = depositMoney;
 buyButton.onclick = buyCola;
 
-
+// 돈 관련 변수에 세 자리마다 ','가 추가된 포맷을 지정해주는 함수
 function setMoneyFormat(){
     balance.innerHTML = parseInt(balance.innerHTML.replace(",", "")).toLocaleString("en-US");
     myMoney.innerHTML = parseInt(myMoney.innerHTML.replace(",", "")).toLocaleString("en-US");
     total.innerHTML = parseInt(total.innerHTML.replace(",", "")).toLocaleString("en-US");
 }
 
+// 획득 버튼을 누르면 구매 리스트에 있는 콜라가 획득 리스트로 이동하는 함수
 function buyCola(){
     // 선택한 콜라의 총 개수 최종 확정
     totalCount = 0;
+    // 구매 리스트의 개수의 총합 구하기
     for(let i = 0; i<buyColaCount.length; i++)
         totalCount += buyColaCount[i];
 
@@ -82,9 +84,9 @@ function buyCola(){
         alert("잔액이 부족합니다. 입금 후 다시 시도해주세요.");
     // 잔액 안 부족하면
     else{
-        // 잔액 = 잔액 - 음료수 구매액
+        // 잔액 = 원래 잔액 - 음료수 구매액
         balance.innerHTML = parseInt(balance.innerHTML.replace(",", "")) - 1000*totalCount;
-        // 총금액 = 총금액 + 음료수 구매액
+        // 총금액 = 원래 총금액 + 음료수 구매액
         total.innerHTML = parseInt(total.innerHTML.replace(",", "")) + 1000*totalCount;
         setMoneyFormat();
 
@@ -94,7 +96,9 @@ function buyCola(){
             // buyColaCount는 초기화
             buyColaCount[i] = 0;
 
+            // 획득 리스트에 콜라 개수 갱신
             setColaCount(ownColaList, ownColaCount, i);
+            // 구매 리스트에 콜라 개수 갱신 (0이 되었으므로 리스트에서 보이지 않게 함)
             setColaCount(buyColaList, buyColaCount, i);
 
             // 콜라 버튼에서 clicked 클래스 제거
@@ -103,28 +107,40 @@ function buyCola(){
     }
 }
 
+// 반환 버튼을 누르면 잔액을 소지금으로 이동
 function returnMoney(){
+    // 소지금 = 원래 소지금 + 잔액
     myMoney.innerHTML = parseInt(myMoney.innerHTML.replace(",", ""))+parseInt(balance.innerHTML.replace(",", ""));
+    // 잔액은 0으로 초기화
     balance.innerHTML = 0;
     setMoneyFormat();
 }
 
+// 입금 버튼을 누르면 입금 input에 입력된 값을 잔액으로 이동
 function depositMoney(){
-    if(parseInt(myMoney.innerHTML.replace(",", "")) >= parseInt(deposit.value)){
+    // 소지금이 입력 input의 value보다 적을 경우
+    if(parseInt(myMoney.innerHTML.replace(",", "")) < parseInt(deposit.value))
+        alert("소지금이 부족합니다. 다시 입력해 주세요.");
+    // 소지금이 입력 input의 value보다 큰 경우
+    else{
+        // 잔액 = 원래 잔액 + 입력 금액
         balance.innerHTML = parseInt(balance.innerHTML.replace(",", "")) + parseInt(deposit.value);
+        // 소지금 = 원래 소지금 - 입력 금액
         myMoney.innerHTML = parseInt(myMoney.innerHTML.replace(",", "")) - parseInt(deposit.value);
+        // 입력 input값 초기화
         deposit.value = "";
         setMoneyFormat();
     }
-    else
-        alert("소지금이 부족합니다. 다시 입력해 주세요.");
 }
 
+// 구매 or 획득 리스트의 값 변경
 function setColaCount(list, countArr, index){
+    // 해당 리스트 원소의 값이 0이라면, display:none인 클래스로 바꿔서 보이지 않게 한다
     if(countArr[index] == 0){
         list.children[index].lastElementChild.classList.remove("selected");
         cola[index].classList.remove("clicked");
     }
+    // 해당 리스트 원소의 값이 0이 아니라면, display를 보이게 하는 클래스를 추가하거나, 원소의 값을 늘린다
     else{
         list.children[index].lastElementChild.lastElementChild.innerHTML = countArr[index];
         list.children[index].lastElementChild.classList.add("selected");
